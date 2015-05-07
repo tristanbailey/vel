92db8e42315db32a2957ec71ef22e9c3bdb9c6c3
Tristan Bailey <tristanbailey@gmail.com>
2015-05-07 16:12:18 +0100

    Update deploy_staging.sh

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -2,7 +2,7 @@
 echo "Beginning deploy_staging.sh"
 env RSYNC_PASSWORD=kdckaucozkws
 export RSYNC_PASSWORD=kdckaucozkws
-rsync -avz -e ssh ./ root@188.226.204.18:/var/www/html/clone/
+rsync -avrz -e "ssh -l root" ./ root@188.226.204.18:/var/www/html/clone/
 
 
 echo "END deploy_staging.sh"
