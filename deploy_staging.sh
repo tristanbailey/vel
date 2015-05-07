cb065cdae934bb88855a1dc0eca17900c6750712
Tristan Bailey <tristanbailey@gmail.com>
2015-05-07 13:07:38 +0100

    Update deploy_staging.sh

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -1,7 +1,7 @@
 
 echo "Beginning deploy_staging.sh"
 
-rsync -r joomla/ root@188.226.204.18:/var/www/html/clone/
+rsync -r ./ root@188.226.204.18:/var/www/html/clone/
 
 
 echo "END deploy_staging.sh"
