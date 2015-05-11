d7791cc6d3311ef701251372d23b00714d1f5c14
Tristan Bailey <tristanbailey@gmail.com>
2015-05-11 10:44:02 +0100

    r not needed as in -a short tag

--- deploy_stag.sh
+++ deploy_stag.sh
@@ -1,5 +1,5 @@
 echo "Beginning deploy_staging.sh"
 
-rsync -avrz --progress -e "ssh -l root" ./ root@188.226.204.18:/var/www/html/clone/
+rsync -avz --progress -e "ssh -l root" ./ root@188.226.204.18:/var/www/html/clone/
 
 echo "END deploy_stag.sh"
